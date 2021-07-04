0296dc4c576a27ef3bd7e47a0284ccea2137e3fa
Andredossantos96 <andresantos216@gmail.com>
2021-07-04 16:38:28 -0300

    updated to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,15 +2,16 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "André"
-    var cpf: String = "123.123.123.11"
+    var cpf: String = "123.456.789.10"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome seu CPF é $cpf"
 
 }
 
 fun main() {
     val andre = Pessoa()
-
-    println(andre)
-    println(andre.nome)
-    println(andre.cpf)
+    println(andre.pessoaInfo())
 }
